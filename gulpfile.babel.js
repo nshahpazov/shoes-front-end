758dd2eb71d8c9fe02defbbd5ec79505b4fc34fb
nshahpazov <nshahpazov@gmail.com>
2016-03-08 17:22:02 +0200

    init

---
+++ gulpfile.babel.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import gulp from 'gulp';
+import tsc from 'gulp-typescript';
+import typescript from 'typescript';
+import browserify from 'gulp-browserify';
+import rename from 'gulp-rename';
+
+// maybe later
+// import sass from 'gulp-sass';
+// import autoprefixer from 'gulp-autoprefixer';
+// import sourcemaps from 'gulp-sourcemaps';
+
+
+
+let tsProject = tsc.createProject('tsconfig.json', {
+  typescript
+});
+
+gulp.task('ts', () => {
+  return gulp.src('./app/src/**/*.ts*')
+    .pipe(tsc(tsProject))
+    .pipe(gulp.dest('./app/dist/'));
+});
+
+gulp.task('browserify', ['ts'], () => {
+  return gulp.src('./app/dist/app.js')
+    .pipe(browserify({
+        debug: true
+    }))
+    .pipe(rename('bundle.js'))
+    .pipe(gulp.dest('./app/dist'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(['./app/src/**', './app/index.html'], ['browserify']);
+});
+
+gulp.task('default', ['browserify']);
