aefd00ef9e1601b6d245e4075eb497c72a40f73d
nshahpazov <nshahpazov@gmail.com>
2016-03-10 19:41:16 +0200

    added a new function for receiving a response from registration

--- app/src/actions/BasketServerActions.ts
+++ app/src/actions/BasketServerActions.ts
@@ -5,5 +5,10 @@ export default {
   receiveBasket(payload) {
     const type = ActionTypes.GET_BASKET_RESPONSE;
     Dispatcher.handleServerAction({ type, payload });
+  },
+
+  receiveBasketRegistration(payload) {
+    const type = ActionTypes.REGISTER_BASKET_RESPONSE;
+    Dispatcher.handleServerAction({ type, payload });
   }
 };
