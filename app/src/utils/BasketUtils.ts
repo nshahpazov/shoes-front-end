d5ec90b85cd16e3469e82d96c1954f203d14c471
nshahpazov <nshahpazov@gmail.com>
2016-03-10 03:10:59 +0200

    init basket api util

--- app/src/utils/BasketUtils.ts
+++ app/src/utils/BasketUtils.ts
@@ -1,19 +1,22 @@
 /// <reference path="../../../typing/superagent.d.ts" />
-import ShoesServerActions from '../actions/ShoesServerActions';
+
+import BasketServerActions from '../actions/BasketServerActions';
 import * as request from 'superagent';
 
 export default {
-  getAllShoes() {
+  getBasket() {
     // todo: change that to api calls
-    // todo: add config routes and default end
-    request.get('http://localhost:8000/shoes.json')
+    // todo: add constants for URIS
+    // todo: add env route
+    // todo: make this to generic api communicator
+    request.get('http://localhost:8000/basket.json')
       .set('Accept', 'application/json')
       .end((err, response) => {
         if (err) {
           return console.error(err);
         }
 
-        ShoesServerActions.receiveShoes(response.body);
+        BasketServerActions.receiveBasket(response.body);
       });
   }
 };
