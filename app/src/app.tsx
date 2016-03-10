d9c10432174e717d7a9c5e7223bb926ad9bed395
nshahpazov <nshahpazov@gmail.com>
2016-03-10 03:00:13 +0200

    added routing

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,10 +1,18 @@
 /// <reference path="../../typing/react.d.ts" />
 /// <reference path="../../typing/react-dom.d.ts" />
+/// <reference path="../../typing/react-router.d.ts" />
 import * as React from 'react';
+import {Router, Route, Link, browserHistory} from 'react-router';
 import * as ReactDOM from 'react-dom';
 import ShoesApp from './components/ShoesApp';
+import Basket from './components/basket/Basket';
 
 ReactDOM.render(
-  <ShoesApp />,
+  <div>
+    <Router history={browserHistory}>
+      <Route path="/" component={ShoesApp} />
+      <Route path="/basket" component={Basket}/>
+    </Router>
+  </div>,
   document.getElementById('main')
 );
