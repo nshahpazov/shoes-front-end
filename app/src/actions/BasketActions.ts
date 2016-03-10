805fa194a28b811f25ec94cc623031dfa1356c5c
nshahpazov <nshahpazov@gmail.com>
2016-03-10 14:08:40 +0200

    changed wrong names

--- app/src/actions/BasketActions.ts
+++ app/src/actions/BasketActions.ts
@@ -4,9 +4,9 @@ import BasketUtils from '../utils/BasketUtils';
 
 export default {
   // add shoe type the param
-  getShoes() {
+  getBasket() {
     const type = ActionTypes.GET_BASKET_RESPONSE;
     Dispatcher.handleViewAction({ type });
-    BasketUtils.getAllShoes();
+    BasketUtils.getBasket();
   }
 };
