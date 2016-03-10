b5ee3fd85c78b2139c79b3bc4c60556ca3531f5e
nshahpazov <nshahpazov@gmail.com>
2016-03-11 01:38:57 +0200

    added cookie basket saving

--- app/src/actions/BasketServerActions.ts
+++ app/src/actions/BasketServerActions.ts
@@ -1,5 +1,7 @@
+/// <reference path="../../../typing/js-cookie.d.ts" />
 import Dispatcher from '../dispatcher/Dispatcher';
 import BasketActionTypes from '../constants/BasketActionTypes';
+import * as Cookies from 'js-cookie';
 
 export default {
   receiveBasket(payload) {
@@ -9,6 +11,11 @@ export default {
 
   receiveBasketRegistration(payload) {
     const type = BasketActionTypes.REGISTER_BASKET_RESPONSE;
+    if (!payload.hasStored) {
+      // todo: make expires being somewhere customly stored (db, config.json file)
+      payload.hasStored = true;
+      Cookies.set('basket', JSON.stringify(payload), {expires: 1});
+    }
     Dispatcher.handleServerAction({type, payload});
   }
 };
