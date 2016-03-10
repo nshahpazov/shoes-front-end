e4e1594abadc0a75e787d5c7844ecc760bf3b397
nshahpazov <nshahpazov@gmail.com>
2016-03-10 23:36:40 +0200

    functionality for registering a basket

--- app/src/utils/BasketUtils.ts
+++ app/src/utils/BasketUtils.ts
@@ -1,12 +1,14 @@
 /// <reference path="../../../typing/superagent.d.ts" />
 /// <reference path="../../../typing/js-cookie.d.ts" />
 /// <reference path="../../../typing/es6-promise.d.ts" />
+/// <reference path="../../../typing/object-assign.d.ts" />
 
 import BasketServerActions from '../actions/BasketServerActions';
 import APIUtils from './APIUtils';
 import * as request from 'superagent';
 import * as Cookies from 'js-cookie';
 import {Promise} from 'es6-promise';
+import assign = require('object-assign');
 
 // change that to customly configured host and inside a module
 // todo: change that to api calls
@@ -14,7 +16,7 @@ import {Promise} from 'es6-promise';
 // todo: add env route
 // todo: make this to generic api communicator
 const URI = 'http://localhost:8000/basket.json';
-const REGISTER_BASKET_URI = 'http://localhost:8000/basket.json';
+const REGISTER_BASKET_URI = 'http://localhost:3000/order';
 
 export default class BasketUtils {
 
@@ -22,17 +24,28 @@ export default class BasketUtils {
     APIUtils.get(URI).then(data => BasketServerActions.receiveBasket(data));
   }
 
+  public static addToBasket(item) {
+
+  }
+
+  // public static registerBasket() {
+  //   return APIUtils.post(REGISTER_BASKET_URI, {
+  //     // todo: remove magic string
+  //     status: 'BASKET'
+  //   });
+  // }
+
   public static registerBasket() {
     const storedBasket = Cookies.get('basket');
-    // todo: use some lib ~~~ _.isUndefined for example
-    const hasStoredBasket = typeof storedBasket !== 'undefined';
-    return new Promise((resolve, reject) => {
-      if (hasStoredBasket) {
-        return storedBasket;
-      } else {
-        return APIUtils.post(REGISTER_BASKET_URI);
-      }
-    });
-    // BasketServerActions.receiveBasketRegistration(data);
+    const status = 'BASKET';
+    const hasStored = typeof storedBasket !== 'undefined';
+    const cookiePromise = () => Promise.resolve(storedBasket);
+    const remotePromise = () => APIUtils.post(REGISTER_BASKET_URI, {status});
+    const promise = hasStored ? cookiePromise() : remotePromise();
+    debugger;
+    promise
+      .then(data => assign(data, {hasStored}))
+      .then(data => {debugger; return data;})
+      .then(BasketServerActions.receiveBasketRegistration);
   }
 };
