8f74bc84baf4f9e2b16c8b0f44b92526cd3a5601
nshahpazov <nshahpazov@gmail.com>
2016-03-11 13:56:42 +0200

    added id for basket loading

--- app/src/actions/BasketActions.ts
+++ app/src/actions/BasketActions.ts
@@ -1,13 +1,17 @@
+/// <reference path="../../../typing/js-cookie.d.ts" />
 import Dispatcher from '../dispatcher/Dispatcher';
 import ActionTypes from '../constants/BasketActionTypes';
 import BasketUtils from '../utils/BasketUtils';
+import * as Cookies from 'js-cookie';
 
 export default {
   // add shoe type the param
   getBasket() {
     const type = ActionTypes.GET_BASKET_RESPONSE;
+    const basketString = Cookies.get('basket');
+    const basket = basketString && JSON.parse(basketString);
     Dispatcher.handleViewAction({type});
-    BasketUtils.getBasket();
+    BasketUtils.getBasket(basket.id);
   },
 
   registerBasket() {
