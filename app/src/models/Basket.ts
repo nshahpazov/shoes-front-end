30c33981c2be992fe30244d94703f67ab293bb43
nshahpazov <nshahpazov@gmail.com>
2016-03-13 00:31:04 +0200

    initial work on the basket class

--- app/src/models/Basket.ts
+++ app/src/models/Basket.ts
@@ -1,5 +1,23 @@
-export default class Basket {
-  constructor() {
 
+/// <reference path="../../../typing/immutable.d.ts" />
+import * as Immutable from 'immutable';
+
+// note: can extend immutable and also have immutable list of records
+export default class Basket {
+
+  private orderItems;
+  private id;
+  private status;
+
+  constructor({orderItems, id, status}) {
+    this.orderItems = orderItems;
+    this.id = id;
+    this.status = status;
+  }
+
+  // note: can return a new basket
+  removeItem(id: number) {
+    const index = this.orderItems.find(el => el.id === id);
+    debugger;
   }
 }
