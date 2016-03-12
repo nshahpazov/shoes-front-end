75d350cdf40460f5a27be84071d41735ec2abb9a
nshahpazov <nshahpazov@gmail.com>
2016-03-13 00:30:48 +0200

    added remove item caller

--- app/src/utils/BasketUtils.ts
+++ app/src/utils/BasketUtils.ts
@@ -10,14 +10,10 @@ import * as Cookies from 'js-cookie';
 import {Promise} from 'es6-promise';
 import assign = require('object-assign');
 
-// change that to customly configured host and inside a module
-// todo: change that to api calls
-// todo: add constants for URIS
-// todo: add env route
-// todo: make this to generic api communicator
 const BASKET_URI = 'http://localhost:3000/orders/';
 const REGISTER_BASKET_URI = 'http://localhost:3000/orders';
-const ADD_ITEM = 'http://localhost:3000/orders/';
+const ITEM_URI = 'http://localhost:3000/orders';
+const status = 'BASKET';
 
 export default class BasketUtils {
 
@@ -29,18 +25,24 @@ export default class BasketUtils {
   public static addToBasket(item) {
     const basket = JSON.parse(Cookies.get('basket'));
     const {id} = basket;
-    const uri = ADD_ITEM + id + '/order_items';
+    const uri = `${ITEM_URI}/${id}/order_items`;
     const data = {size: item.size, shoeModelId: item.id};
 
     APIUtils.post(uri, data)
       .then(BasketServerActions.receiveBasketItemPush);
   }
 
+  public static removeItem(basketId, itemId) {
+    const uri = `${ITEM_URI}/${basketId}/order_items/${itemId}`;
+    APIUtils
+      .delete(uri)
+      .then(BasketServerActions.receiveBasketItemRemoval);
+  }
+
   public static registerBasket() {
     const basketAsString = Cookies.get('basket');
     const hasStored = typeof basketAsString !== 'undefined';
     const basket = hasStored && JSON.parse(basketAsString);
-    const status = 'BASKET';
     const cookiePromise = () => Promise.resolve(basket);
     const remotePromise = () => APIUtils.post(REGISTER_BASKET_URI, {status});
     const promise = hasStored ? cookiePromise() : remotePromise();
