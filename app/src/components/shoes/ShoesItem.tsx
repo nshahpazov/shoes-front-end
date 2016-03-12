3cfeda981ffdfd6c3410711f5872d8939ba29534
nshahpazov <nshahpazov@gmail.com>
2016-03-13 00:34:55 +0200

    refactored to stateless functional components, made shoes into a ul>li list

--- app/src/components/shoes/ShoesItem.tsx
+++ app/src/components/shoes/ShoesItem.tsx
@@ -4,29 +4,20 @@ import * as React from 'react';
 import {Tooltip, OverlayTrigger} from 'react-bootstrap';
 import ShoesActions from '../../actions/ShoesActions';
 
-export default class ShoesItem extends React.Component<any, any> {
-  constructor(props) {
-    super(props);
-  }
+export default ({shoes}) => {
+  const {name, description, price} = shoes;
+  const tooltip = (<Tooltip>{description}</Tooltip>);
 
-  onClickAdd(item) {
-    ShoesActions.addToBasket(item);
-  }
-
-  render() {
-    const {name, description, price} = this.props.shoes;
-    const tooltip = (<Tooltip>{description}</Tooltip>);
-
-    return (
-      <div className="shoes-item">
-        <p className="shoes-item-name">{name} - ${price}</p>
-        <OverlayTrigger placement="bottom" overlay={tooltip}>
-          <img src="images/shoes.jpg"/>
-        </OverlayTrigger>
-        <button className="btn btn-default" onClick={this.onClickAdd.bind(this, this.props.shoes)}>
-          Add to Basket
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="shoes-item">
+      <p className="shoes-item-name">{name} - ${price}</p>
+      <OverlayTrigger placement="bottom" overlay={tooltip}>
+        <img src="images/shoes.jpg"/>
+      </OverlayTrigger>
+      <button className="btn btn-default"
+        onClick={() => ShoesActions.addToBasket(shoes)}>
+        Add to Basket
+      </button>
+    </div>
+  );
+};
--- app/src/components/shoes/ShoesList.tsx
+++ app/src/components/shoes/ShoesList.tsx
@@ -2,15 +2,7 @@
 import * as React from 'react';
 import ShoesItem from './ShoesItem';
 
-export default class ShoesList extends React.Component<any, any> {
-
-  constructor(props: any) {
-    super(props);
-  }
-
-  render() {
-    const {shoes} = this.props;
-    const list = shoes.map(shoes => <ShoesItem shoes={shoes} />);
-    return ( <div> {list} </div> );
-  }
+export default ({shoes}) => {
+  const list = shoes.map(shoes => <ShoesItem shoes={shoes} />);
+  return ( <div> {list} </div> );
 }
