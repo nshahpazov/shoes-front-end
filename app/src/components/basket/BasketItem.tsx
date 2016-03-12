b438b136b133a22b3790ab86399b0c0ec4b0f4e7
nshahpazov <nshahpazov@gmail.com>
2016-03-13 00:34:19 +0200

    made into stateless functional components and added reference of the basket

--- app/src/components/basket/BasketItem.tsx
+++ app/src/components/basket/BasketItem.tsx
@@ -1,27 +1,25 @@
 /// <reference path="../../../../typing/react.d.ts" />
 /// <reference path="../../../../typing/react-bootstrap.d.ts" />
+
 import * as React from 'react';
 import {Tooltip, OverlayTrigger} from 'react-bootstrap';
+import BasketActions from '../../actions/BasketActions';
 
-export default class BasketItem extends React.Component<any, any> {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const {name, description, price} = this.props.item;
-    const tooltip = (<Tooltip>{description}</Tooltip>);
-
-    return (
-      <div>
-        <p>{name}</p>
-        <button type="button" className="close"></button>
-        <OverlayTrigger placement="bottom" overlay={tooltip}>
-          <img src="images/shoes.jpg"/>
-        </OverlayTrigger>
-        <p>PRICE: {price} </p>
-      </div>
-    );
-  }
+export default ({item, basket}) => {
+  const {name, description, price} = item.shoeModel;
+  const tooltip = (<Tooltip>{description}</Tooltip>);
+  return (
+    <div>
+      <p>
+        {name} &nbsp;
+        <button onClick={() => BasketActions.removeItem(basket, item)}>
+          X
+        </button>
+      </p>
+      <OverlayTrigger placement="bottom" overlay={tooltip}>
+        <img src="images/shoes.jpg" />
+      </OverlayTrigger>
+      <p>PRICE: {price} </p>
+    </div>
+  );
 }
--- app/src/components/basket/BasketItemList.tsx
+++ app/src/components/basket/BasketItemList.tsx
@@ -2,19 +2,9 @@
 import * as React from 'react';
 import BasketItem from './BasketItem';
 
-export default class BasketList extends React.Component<any, any> {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const {items} = this.props;
-    debugger;
-    // todo: make this ul > li
-    const itemList = items.map(item => item.shoeModel)
-                          .map(item => <BasketItem item={item} />);
-
-    return (<div> {itemList} </div>);
-  }
+export default ({items, basket}) => {
+    const itemList = items.map(item => (<li>
+      <BasketItem item={item} basket={basket}/>
+    </li>));
+    return (<div><ul> {itemList}</ul></div>);
 }
